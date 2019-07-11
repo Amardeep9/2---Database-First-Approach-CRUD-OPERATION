398486ac324bbdcc44df4f51188e7379f688af63
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-07-11 17:22:20 +0530

    Add files via upload

---
+++ AutoChangeTracking.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    class AutoChangeTracking
+    {
+        public static void Main(string[] args)
+        {
+            MyDbEntities db = new MyDbEntities();
+
+            Area newArea = new Area() { AreaName = "Oxford Street", CityId = 4, Pincode = "123456" };
+            db.Areas.Add(newArea);
+            Console.WriteLine("After Adding");
+            foreach (var tracker in db.ChangeTracker.Entries<Area>())
+            {
+                Console.WriteLine(tracker.State);
+            }
+
+
+
+            Area modifiedArea = db.Areas.Find(3);
+            if (modifiedArea != null)
+            {
+                modifiedArea.Pincode = "124522";
+           
+
+            Console.WriteLine("After Modification");
+            foreach (var tracker in db.ChangeTracker.Entries<Area>())
+            {
+                Console.WriteLine(tracker.State);
+            }
+            }
+
+            Area delArea = db.Areas.Find(1);
+            if (delArea != null)
+            {
+                db.Areas.Remove(delArea);
+            }
+
+
+            Console.WriteLine("After Deletion");
+            foreach (var tracker in db.ChangeTracker.Entries<Area>())
+            {
+                Console.WriteLine(tracker.State);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EagerLoading.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    class EagerLoading
+    {
+        public static void Main(string[] args)
+        {
+            MyDbEntities db = new MyDbEntities();
+            foreach (City cobj in db.Cities.Include("Areas"))
+            {
+                Console.WriteLine("{0}\t{1}", cobj.CityId, cobj.CityName);
+                foreach (Area aobj in cobj.Areas)
+                {
+                    Console.WriteLine(aobj.AreaName);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
